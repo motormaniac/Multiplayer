363f049542eb187ef20fdeb80ad6c5f46f646fe4
motormaniac <darrienlee626@gmail.com>
2024-11-08 09:54:27 -0500

    server structure modified

---
+++ server/src/scripts/main.js
@@ -0,0 +1,8 @@
+"use strict";
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+console.log(process.env.PORT);
+// app.listen(8080, ()=>{
+//     console.log("Listening on port 8080")
+// })
---
+++ server/src/scripts/main.ts
@@ -0,0 +1,9 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+
+console.log(process.env.PORT)
+
+// app.listen(8080, ()=>{
+//     console.log("Listening on port 8080")
+// })
